026d88f9bc7bdff4719b9b137f2cf32e265f98da
bedel-dev <bedelcoulibaly8@gmail.com>
2023-03-30 11:38:59 +0000

    up

--- src/app/@shared/components/header/header-operation/header-operation.component.ts
+++ src/app/@shared/components/header/header-operation/header-operation.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from 'src/app/@core/services/auth.service';
 import { LANGUAGES } from 'src/config/language-config';
 import { User } from '../../../models/user';
 import { I18nService } from 'ng-devui/i18n';
+import { ListDataService } from 'src/app/@core/mock/list-data.service';
 
 @Component({
   selector: 'da-header-operation',
@@ -18,12 +19,17 @@ export class HeaderOperationComponent implements OnInit {
   haveLoggedIn = false;
   noticeCount!: number;
 
-  constructor(private route: Router, private authService: AuthService, private translate: TranslateService, private i18n: I18nService) {}
+  constructor(private listDataService: ListDataService,private route: Router, private authService: AuthService, private translate: TranslateService, private i18n: I18nService) {}
 
   ngOnInit(): void {
     if (localStorage.getItem('userinfo')) {
       this.user = JSON.parse(localStorage.getItem('userinfo')!);
       this.haveLoggedIn = true;
+      this.getAllNotification()
+
+      setInterval(() => {
+        this.getNewnotification()
+      }, 2000);
     } else {
       this.authService.logout();
       this.route.navigate(['/', 'login']);
@@ -38,6 +44,34 @@ export class HeaderOperationComponent implements OnInit {
     this.onLanguageClick("en-us")
   }
 
+  getNewnotification(){
+    this.listDataService.getListAllData("list.php","notif").subscribe((notification:any)=>{
+       notification.response.data.forEach((element:any) => {
+        if(element.state =="non lu"){
+          var notif =   this.AllNotification.filter((n:any)=>{
+            return n.id.toString()==element.id.toString()
+          })
+          if(notif.length==0){
+            this.AllNotification.push(element)
+          }
+          //console.log(notif)
+        }
+       });
+       //console.log("notif",notification.response.data)
+    })
+  }
+  AllNotification:any[]=[];
+  getAllNotification(){
+    this.AllNotification = [];
+    this.listDataService.getListAllData("list.php","notif").subscribe((notification:any)=>{
+       notification.response.data.forEach((element:any) => {
+        if(element.state =="non lu"){
+          this.AllNotification.push(element)
+        }
+       });
+       console.log("notif",notification.response.data)
+    })
+  }
   onSearch(event: any) {
     console.log(event);
   }
